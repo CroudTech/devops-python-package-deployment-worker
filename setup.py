0566beabc23372d87c24c39cd1ffea22f3315b3d
Jim Robinson <jscrobinson@gmail.com>
2020-06-10 13:57:30 +0100

    Use python 3.6

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.8",
+    python_requires=">=3.6",
     entry_points={
         "console_scripts": ["croudtech-helm-helper=croudtech_helm_helper.cli:main"],
     },
